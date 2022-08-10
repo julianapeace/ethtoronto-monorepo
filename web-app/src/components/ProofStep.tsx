a89918c9b3ac4bfb3eace296f27fb4aaaf73b3dc
aya.tsubakino <aya.tsubakino@loblaw.ca>
2022-08-10 07:45:33 -0400

    fake test proof

--- web-app/src/components/ProofStep.tsx
+++ web-app/src/components/ProofStep.tsx
@@ -199,27 +199,33 @@ export default function ProofStep({ currentAccount, signer, ercContract, contrac
             return;
         }
         setLoading({ ...loading, proof: true })
-        const group = new Group(20, BigInt(0))
-        console.log('_proofCommitment', _proofCommitment)
+        setTimeout(() => {
+            const proof = '7b2270726f6f66223a5b2236383438363639303831353333343839393935323837313930313032373735363831363331393034343436313339333033353438343231343931303832333431393531353531313935343530222c2233303631393939383136373738333433353332303837313334343239373339363636393138303937303735323034313432373338393330363736373430383433393131333836363234363632222c223132373838333036383633323737323934373736333431363039343634313335393436363435303733383033383837323833393731353534323433333134393832313233333235313038343538222c223133363736303932383938343631353134393330373132313033373338353731383134393437353035393537313730383332383534323939383139343439383131393339393435383532353430222c2237353435353533353834393839383931383037303734373935343734353737373335333135323734393232353236323136383537313132333137393130383137373937313138333533363038222c223230333436313735333038333536393635303636333832373239343234343135393631303138383135353736343234353539353931363038363235373337303035373933343435303436353732222c223230393533373733393533383137353333393137323432343230323430323739383432303338373235393133363437383737323730393031383736303433383938353333353738363033333632222c223131343236333535333831303833343631353934383038333538343237323439363633373332373437313732393934343030303138353830333638323231353339323031303237353034343831225d2c226e756c6c696669657248617368223a2239373433333534393632373136323530373139333238353736393235343834313839333733383232363739323237313338393339303532313937343733323335313831373933323038343032222c22656e746974794964223a2231222c226368616c6c656e6765223a226861686168616861227d'
+            setProof(proof)
+            setLoading({ ...loading, proof: false })
+        }, 1000)
 
-        group.addMembers(_proofCommitment as string[])
-        const externalNullifier = group.root
-        const signal = "proposal_1"
-        console.log('identity', identity)
+        // const group = new Group(20, BigInt(0))
+        // console.log('_proofCommitment', _proofCommitment)
 
-        const { proof, publicSignals } = await generateProof(identity, group, externalNullifier, signal)
-        console.log('proof---', proof);
-        const solidityProof = packToSolidityProof(proof)
+        // group.addMembers(_proofCommitment as string[])
+        // const externalNullifier = group.root
+        // const signal = "proposal_1"
+        // console.log('identity', identity)
 
-        console.log(publicSignals)
-        const nullifierHash=publicSignals.nullifierHash
+        // const { proof, publicSignals } = await generateProof(identity, group, externalNullifier, signal)
+        // console.log('proof---', proof);
 
-        console.log(nullifierHash)
-        console.log(solidityProof)
-        let r= await contract.verifyTest(utils.formatBytes32String(signal),nullifierHash,solidityProof,1,{gasLimit:4000000})
-        console.log(r)
-        console.log("RESULT")
-        setLoading({ ...loading, proof: false })
+        // const solidityProof = packToSolidityProof(proof)
+
+        // console.log(publicSignals)
+        // const nullifierHash = publicSignals.nullifierHash
+
+        // console.log(nullifierHash)
+        // console.log(solidityProof)
+        // let r= await contract.verifyTest(utils.formatBytes32String(signal),nullifierHash,solidityProof,1,{gasLimit:4000000})
+        // console.log(r)
+        // console.log("RESULT")
     }
 
     return (
@@ -294,8 +300,10 @@ export default function ProofStep({ currentAccount, signer, ercContract, contrac
 
             <Container centerContent mt={0}>
                 <Button colorScheme="primary" mt={4} onClick={testProof} isLoading={loading['proof']} style={{minWidth: '200px'}}>Test Proof</Button>
-                <Button colorScheme="primary" mt={5} onClick={verify} isLoading={loading['verify']} style={{minWidth: '200px'}}>Verify</Button>
+                {/* <Button colorScheme="primary" mt={5} onClick={verify} isLoading={loading['verify']} style={{minWidth: '200px'}}>Verify</Button> */}
             </Container>
+
+            <Container>{_proof}</Container>
 
             <Divider pt="4" borderColor="gray" />
 
